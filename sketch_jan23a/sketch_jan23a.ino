1ab87be8a769a901a92bc11ddb5c4d7c8cddb30b
lalit184 <lalitsrinivas1@gmail.com>
2018-01-31 18:55:38 +0530

    corrections for general callibration

--- sketch_jan23a/sketch_jan23a.ino
+++ sketch_jan23a/sketch_jan23a.ino
@@ -2,13 +2,12 @@
 float integral=0,differential=0,proportional=0;
 float previous_error=0;
 int current_val=0;
-const int zero_bit_val=0;// add relevant data
+const int zero_bit_val=0;// add relevant data only if the apparatus is incorrectly callibrated
 
 const int analog_in=A0;//add relevant data
-const int analog_out_1=A10;//add relevant data
-const int analog_out_2=A9;//add relevant data
-const int kp=0,kd=0,ki=0;//add relevant data
-const float slope=360.0/1023.0;//add relevant data
+const int analog_out_1=10;//add relevant data
+const int analog_out_2=9;//add relevant data
+const int kp=1,kd=1,ki=1;//add relevant data
 
 int mapped_value;
 float goal;
@@ -16,32 +15,19 @@ float scaling_factor=0;//add relevant data
 int int_cost;
 
 float cost;
-int inertial_threshold=0;//add relevant data
+int inertial_threshold=30;//add relevant data
 
 int map_val(int x)
-{
-  int y;
-  if(x>zero_bit_val)
-    {
-      y=x+zero_bit_val;
-    }
-  else 
-  {
-    y=x-zero_bit_val;
-  }
-  return y;
-}
-
-int map_val_general(int x)
 {
   int y;
   y=(x+1023-zero_bit_val)%1023;
   return y;
 }
 
+
 int clip(int x)
 {
-  if(x>150)                // you can change this as per requirement of smaller response time 
+  if(x>255)                // you can change this as per requirement of smaller response time 
   {
     x=255;
   }
@@ -58,21 +44,16 @@ void setup()
  current_val=analogRead(analog_in);
  mapped_value=map_val(current_val);
  
- if(mapped_value>512)
-  {
-    goal=512+180*slope;
-  }
- else
-  {
-    goal=512-180*slope;
-  }
-  
+ pinMode(analog_out_1, OUTPUT);
+ pinMode(analog_out_2, OUTPUT);
+
+ goal=(mapped_value+512)%1023;
   // put your setup code here, to run once:
 
 }
 
 void loop()
-{
+{                                                                                                                             
   current_val=analogRead(analog_in);
   mapped_value=map_val(current_val);
   
@@ -82,22 +63,25 @@ void loop()
   differential=proportional-previous_error;
 
   cost=kp*proportional+ki*integral+kd*differential;
-  cost*=scaling_factor;
+ // cost*=scaling_factor;
   int_cost=clip(abs(cost));
+  
+  Serial.println(int_cost);
   
   if(cost>0)
   {
-    // clockwise rotation this could be different later 
+    // execute rotation to increase angle place pin 9 and 10 correspondingly
     analogWrite(analog_out_1,int_cost);
     analogWrite(analog_out_2,0);
     
   }
   if(cost<0)
   {
-    // anti-clockwise rotation this could be different later 
+    // execute rotation to decrease angle place pin 9 and 10 correspondingly
     analogWrite(analog_out_2,int_cost);
     analogWrite(analog_out_1,0);
     
   }
   
 }
+
