fea6f84d26b3a8b1746fc1c8f2e06023c664a98a
lalit184 <lalitsrinivas1@gmail.com>
2018-02-02 17:52:23 +0530

    important spec info and code info

--- sketch_jan23a/sketch_jan23a.ino
+++ sketch_jan23a/sketch_jan23a.ino
@@ -2,22 +2,22 @@
 float integral=0,differential=0,proportional=0;
 float previous_error=0;
 int current_val=0;
-const int zero_bit_val=0;// add relevant data only if the apparatus is incorrectly callibrated
 
-const int analog_in=A0;//add relevant data
-const int analog_out_1=9;//add relevant data
-const int analog_out_2=10;//add relevant data
-const int kp=1,kd=1,ki=1;//add relevant data
+const int analog_in=A0;//input pin for analog read from potentiometer
+const int analog_out_1=9;//this is for control signal for gnd port of motor and anti-clockwise rotation
+const int analog_out_2=10;//this is for control signal for 12v port of motor and clockwise rotation
+const int kp=2.8,kd=14,ki=0.1;//control system constants
 
-int mapped_value;
-float goal;
-float scaling_factor=0;//add relevant data
-int int_cost;
+int mapped_value; //remapping bitvalue if angle vs analog read is discontinuous 
+float goal; // always 180 degrees from current or +512 bits
+
+
 
 float cost;
-int inertial_threshold=30;//add relevant data
+int int_cost; // (int)cost
+int inertial_threshold=45;//minimal control signal for achieving motion of motor 
 
-int map_val(int x)
+int map_val(int x)  //generates a continuos map between angle and bit value
 {
   int y;
   y=(x+1023-zero_bit_val)%1023;
@@ -27,7 +27,7 @@ int map_val(int x)
 
 int clip(int x)
 {
-  if(x>255)                // you can change this as per requirement of smaller response time 
+  if(x>150)                // you can change this as per requirement of smaller response time 
   {
     x=255;
   }
@@ -48,7 +48,7 @@ void setup()
  pinMode(analog_out_2, OUTPUT);
 
  goal=(mapped_value+512)%1023;
-  // put your setup code here, to run once:
+  
 
 }
 
@@ -70,18 +70,20 @@ void loop()
   
   if(cost>0)
   {
-    // execute rotation to increase angle place pin 9 and 10 correspondingly
+    // execute anticlockwise rool i.e decrease angle 
     analogWrite(analog_out_1,int_cost);
     analogWrite(analog_out_2,0);
     
   }
   if(cost<0)
   {
-    // execute rotation to decrease angle place pin 9 and 10 correspondingly
+    // execute clockwise roll i.e increase angle 
     analogWrite(analog_out_2,int_cost);
     analogWrite(analog_out_1,0);
-    
+
   }
+
+
   
 }
 
